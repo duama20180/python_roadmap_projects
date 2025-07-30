6c66583cc7b0a7cbf50a65e641e28a9dfa46af6a
duama20180 <accforwork20180@gmail.com>
2025-07-30 12:25:44 +0300

    half of functions of number guessing game are done

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -17,3 +17,28 @@ def get_difficulty():
         except ValueError:
             print("Invalid input. Please enter a number 1, 2 or 3")
 
+def get_chances(difficulty):
+    return {1:10, 2:5, 3:3}[difficulty]
+
+def get_difficulty_name (difficulty):
+    return {1:"Easy", 2:"Medium", 3:"Hard"}[difficulty]
+
+def get_guess(guess):
+    while True:
+        try:
+            guess = int(input("Please enter your guess:"))
+            if 1 <= guess <= 100:
+                return guess
+            print("Please enter a number between 1 and 100.")
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 100.")
+
+def provide_hint(guess, target, remaining):
+    if remaining > 0:
+        if guess > target:
+            print(f"Incorrect guess. The number is less than {guess}. You have {remaining} remaining.")
+        else:
+            print(f" Incorrect guess. The number is greater than {guess}. You have {remaining} remaining.")
+        if remaining >= 3 and (target % 2 == 0 and guess % 2 != 0 or target % 2 != 0 and guess % 2 == 0):
+            print("Hint: The number's parity is different from your guess.")
+
